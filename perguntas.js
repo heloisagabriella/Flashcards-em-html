833fa2a17606d5e48de4f625b4eb5a0166cfba9d
heloisagabriella <heloisa.gabriela.silva@escola.pr.gov.br>
2025-10-10 07:58:54 -0300

    aula10/10

---
+++ perguntas.js
@@ -0,0 +1,14 @@
+criaCartao(
+    'programação
+    'o que é python?',
+    'o python é uma linguagem de programação')
+    criaCartao(
+        'geografia',
+        'qual a capital da frança?'
+        'a capital da frança é paris'
+    )
+    criaCartao(
+        'programação'
+        'o que é uma função?'
+        'uma função é um bloco de codigo que executa alguma tarefa'
+    )
